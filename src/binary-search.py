d3ba02f6bf363f4a0994d67a88480748f4456739
Robson da Silva <me@rsilva.net>
2019-01-07 23:05:42 -0200

    Print concats fixed

--- src/binary-search.py
+++ src/binary-search.py
@@ -20,7 +20,7 @@ def search(list, item):
 my_list = [1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21]
 
 position, step = search(my_list, 11)
-print("Item 7 was found on #", position, "with", step, "steps")
+print("Item 7 was found on #" + `position` + " with " + `step` + " steps")
 
 position, step = search(my_list, 21)
-print("Item 21 was found on #", position, "with", step, "steps")
+print("Item 21 was found on #" + `position` + " with " + `step` + " steps")
