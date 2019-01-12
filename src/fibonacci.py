677f54895449fb5bbfc923d682468bcf0f20d554
Robson da Silva <me@rsilva.net>
2019-01-11 23:58:16 -0200

    Fibonacci recursive implemented

--- src/fibonacci.py
+++ src/fibonacci.py
@@ -1,7 +1,20 @@
-iterations = 20
-items = [0, 1]
+def fibo(iterations):
+    items = [0, 1]
+    for i in range(iterations - 2):
+        items.append(items[i] + items[i + 1])
+    return items
 
-for i in range(iterations):
-    items.append(items[i] + items[i + 1])
+def fiboRecursiveAux(x, y, items, iterations):
+    items.append(x)
+    if len(items) == iterations:
+        return
+    fiboRecursiveAux(y, x + y, items, iterations)
 
-print(items)
+
+def fiboRecursive(iterations):
+    items = []
+    fiboRecursiveAux(0, 1, items, 20)
+    return items    
+
+print(fibo(20))
+print(fiboRecursive(20))
