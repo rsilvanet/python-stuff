ad429e827f993ecf4c53a9f9dee5cf4e5dc889d0
Robson da Silva <me@rsilva.net>
2019-05-22 23:36:12 -0300

    Polymorphism sample code added

---
+++ src/polymorphism.py
@@ -0,0 +1,45 @@
+class Door:
+    color = 'colorless'
+    
+    def __init__(self, color):
+        self.color = color
+
+class House:
+    door = None
+    area = 0
+    description = ''
+    
+    def __init__(self, door, area):
+        self.door = door
+        self.area = area
+        self.description = 'a house'
+    
+    def describe(self):
+        return 'a ' + `self.area` + 'm2 ' + self.description + ' with ' + self.door.color + ' doors'
+
+class Apartment(House):
+    def __init__(self, door):
+        self.door = door
+        self.area = 50
+        self.description = 'an apartment'
+
+class Person:
+    name = ''
+    home = None
+
+    def __init__(self, name, home):
+        self.name = name
+        self.home = home
+
+    def describe(self):
+        return 'My name is ' + self.name + ' and I live in ' + self.home.describe()
+
+door = Door('black')
+house = House(door, 75)
+apartment = Apartment(door)
+
+person1 = Person('John', house)
+person2 = Person('Mary', apartment)
+
+print(person1.describe())
+print(person2.describe())
